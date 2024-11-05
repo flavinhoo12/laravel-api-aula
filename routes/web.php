c7892263e2d2fc0b9df824ed3f78c5a980daecc9
theflash12 <scazzi12@gmail.com>
2024-11-05 09:01:52 -0300

    JWT - json web token
    aula 05-11

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,8 @@ Route::prefix('/people')->group(function(){
 
     Route::post('/store', [PeopleController::class, 'store']);
 });
+
+Route::prefix('/user')->group(function(){
+    Route::post('/register', [JWTAuthController::class, 'register']);   
+});
+
